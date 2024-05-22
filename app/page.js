03a107c5fcbaf24e4ac64d5c10367fae47fc101d
Akshat Rawat <akshatrawat84@gmail.com>
2024-05-22 11:56:44 +0530

    Add 2-Way Binding in description Section

--- app/page.js
+++ app/page.js
@@ -16,17 +16,29 @@ const page = () => {
     <form>
       <input type='text' 
       className='text 2xl border-zinc-800 border-4 m-5 px-4 py-2 '
-      placeholder='Enter title here ! ' // That thing is display on that box when there is nothing writtern
-      value={title} 
-      onChange={(e)=>{
+      placeholder='Enter Title here ! ' // That thing is display on that box when there is nothing writtern
+      
+      //2-Ways Binding 
+      // Here I am doing 2-way binding . It means that tell the data to the react and also show the data to the user .
+
+      value={title}  
+      onChange={(e)=>{ 
         // console.log(e.target.value)
         settitle(e.target.value)
       }}
+      
       />
 
       <input type='text' 
       className='text 2xl border-zinc-800 border-4 m-5 px-4 py-2 '
       placeholder='Add Description here... ' 
+
+      value={desc}  
+      onChange={(e)=>{ 
+        // console.log(e.target.value)
+        settitle(e.target.value)
+      }}
+
       />
 
       <button className='bg-black text-white px-4 py-3 m-5 font-bold text-2xl rounded'> Add Task </button>
