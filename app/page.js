4c534abd4493c19b2d2e416bfd4475b46b60ec11
Akshat Rawat <akshatrawat84@gmail.com>
2024-05-22 11:33:21 +0530

    Add Form Section
    1. Add 2 Input Blocks . One is for the Task and Another is for the
       Description .
    2. Add Button on it .

--- app/page.js
+++ app/page.js
@@ -5,6 +5,24 @@ const page = () => {
     //Fragerant ( Empty Tag )
     <>
       <h1 className='bg-black text-white p-5 text-center font-bold text-5xl'> Akshat's TO DO LIST </h1>
+   
+
+    <form>
+      <input type='text' 
+      className='text 2xl border-zinc-800 border-4 m-5 px-4 py-2 '
+      placeholder='Enter Task here ! ' // That thing is display on that box when there is nothing writtern 
+      />
+
+      <input type='text' 
+      className='text 2xl border-zinc-800 border-4 m-5 px-4 py-2 '
+      placeholder='Add Description here... ' 
+      />
+
+      <button className='bg-black text-white px-4 py-3 m-5 font-bold text-2xl rounded'> Add Task </button>
+
+    </form>
+
+    
     </>
   )
 }
